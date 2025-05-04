dda104d0eda90e4f0e21cde98024edd8400352f8
VallNB <leo.dalgarno@roboroos.org.au>
2025-05-04 03:01:56 +0000

    May 4, 2025, 03:01:56 UTC

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.get('/quiz.html', function(req, res) {
+
+
+});
+
+
 module.exports = router;
