f370dc71d424ac7d5a1efa73aad1e4e380e90115
VallNB <leo.dalgarno@roboroos.org.au>
2025-05-05 05:54:12 +0000

    May 5, 2025, 05:54:12 UTC

--- public/javascripts/quiz.js
+++ public/javascripts/quiz.js
@@ -126,22 +126,10 @@ function getRandQuizMon(callback) {
   }
 
   function validate(callback) {
-    var xhttp = new XMLHttpRequest();
-
-    xhttp.onreadystatechange = function() {
-        if (this.responseText != "") {
-            //console.log(this.responseText)
-            var data = JSON.parse(this.responseText);
-            //console.log(data.results[Math.floor(Math.random()*1025)].name);
-            callback(data.results[Math.floor(Math.random()*1025)].name);
-
-            // Quiz Parameters
-            if (score < 3) {
-
-            }
-        }
-    };
-
-    xhttp.open("GET", "https://pokeapi.co/api/v2/pokemon?limit=1025&offset=0", true);
-    xhttp.send();
+    console.log("guess")
+    if (document.getElementById('search').value == quizmon) {
+        score++;
+        console.log("correct");
+        callback(questionGen);
+    } else 
   }
