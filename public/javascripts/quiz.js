e16dcb424e79cb03ccaf468316eb905ef2dd52c0
VallNB <leo.dalgarno@roboroos.org.au>
2025-05-05 01:21:59 +0000

    May 5, 2025, 01:21:59 UTC

--- public/javascripts/quiz.js
+++ public/javascripts/quiz.js
@@ -18,8 +18,31 @@ function quizgen() {
 
 }
 
+var num = 0;
+function quizgen() {
+    num++;
+    var element = document.getElementById('randsprite');
+
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        // Overcomplicated the heck out of this
+        var test = getAPI();
+        console.log(test);
+        console.log(num);
+        element.src = this.responseText;
+    };
+
+    xhttp.open("GET", "https://pokeapi.co/api/v2/pokemon/ditto/sprites/front_default", true);
+    xhttp.send();
+
+}
+
+
+
+/*
 function getAPI() {
-    const apiUrl = 'https://pokeapi.co/api/v2/pokemon/ditto';
+    const apiUrl = 'https://pokeapi.co/api/v2/pokemon/ditto/sprites/front_default';
     var test = ""
     fetch(apiUrl)
     .then(response => {
@@ -40,3 +63,4 @@ function getAPI() {
 
     return test;
 }
+    */
