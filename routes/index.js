39e41b930222a6acc7fa4f52619e95cbf6dc96f3
VallNB <leo.dalgarno@roboroos.org.au>
2025-05-05 06:14:36 +0000

    May 5, 2025, 06:14:36 UTC

--- routes/index.js
+++ routes/index.js
@@ -9,23 +9,6 @@ router.get('/', function(req, res, next) {
 
 router.get('/quiz.txt', function(req, res) {
   const apiUrl = 'https://pokeapi.co/api/v2/pokemon/ditto';
-  /*
-  // Make a GET request
-  fetch(apiUrl)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log(data.sprites.front_default);
-      res.send(data.sprites.front_default);
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
-    */
 
     res.send("./images/Test.png");
 
